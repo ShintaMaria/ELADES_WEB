a9885787ef9084862d2e0b7a91e1598ae7001683
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-03-05 21:52:36 +0700

    Create KabarDesa.php

---
+++ Elades_Web/app/Models/KabarDesa.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KabarDesa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'kabar_desa'; // Sesuaikan dengan nama tabel di database
+
+    protected $fillable = [
+        'judul', 
+        'deskripsi', 
+        'tanggal', 
+        'gambar'
+    ];
+}
