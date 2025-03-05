e6b0d174fd3c7067c730b2bb29242de7174f24f5
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-03-05 21:52:45 +0700

    Update KabarDesaController.php

--- Elades_Web/app/Http/Controllers/informasi/KabarDesaController.php
+++ Elades_Web/app/Http/Controllers/informasi/KabarDesaController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers\informasi;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\KabarDesa; // Pastikan model ini dipanggil
+
 class KabarDesaController extends Controller
 {
     public function index()
     {
-        return view('informasi.kabar_desa');
+        // Ambil semua data dari tabel kabardesa
+        $kabars = KabarDesa::all();
+
+        // Kirim data ke view
+        return view('informasi.kabar_desa', compact('kabars'));
     }
 }
