856bc4bae67572e1262ac06d13f41cd5b95fb6e6
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-03-10 21:52:47 +0700

    Create edit.blade.php

---
+++ Elades_Web/resources/views/informasi/artikel/edit.blade.php
@@ -0,0 +1,24 @@
+@extends('dashboard.layouts.template')
+
+@section('content')
+<div class="container">
+    <h1>Edit Artikel</h1>
+    <form action="{{ route('artikels.update', $artikel->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="mb-3">
+            <label for="judul" class="form-label">Judul</label>
+            <input type="text" class="form-control" id="judul" name="judul" value="{{ $artikel->judul }}" required>
+        </div>
+        <div class="mb-3">
+            <label for="isi" class="form-label">Deskripsi</label>
+            <textarea class="form-control" id="isi" name="isi" required>{{ $artikel->isi }}</textarea>
+        </div>
+        <div class="mb-3">
+            <label for="link" class="form-label">Link</label>
+            <input type="url" class="form-control" id="link" name="link" value="{{ $artikel->link }}">
+        </div>
+        <button type="submit" class="btn btn-primary">Update</button>
+    </form>
+</div>
+@endsection
