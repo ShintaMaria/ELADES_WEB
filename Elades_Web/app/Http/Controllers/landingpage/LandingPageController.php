4998bf0c3b41f30018ebda84b24df252a6470b0b
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-04-07 20:14:36 +0700

    Update LandingPageController.php

--- Elades_Web/app/Http/Controllers/landingpage/LandingPageController.php
+++ Elades_Web/app/Http/Controllers/landingpage/LandingPageController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers\landingpage;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Artikel; // Panggil model Artikel
+use App\Models\Statistik;
 class LandingPageController extends Controller
 {
     public function index()
     {
-        $artikels = Artikel::latest()->take(4)->get(); // Ambil 4 artikel terbaru
-        return view('landingpage.landing_page', compact('artikels'));
+        $artikels = Artikel::latest()->take(4)->get();
+        $statistik = Statistik::first(); // atau sesuai ID yang kamu gunakan
+
+        return view('landingpage.landing_page', compact('artikels', 'statistik'));
     }
+
 }
