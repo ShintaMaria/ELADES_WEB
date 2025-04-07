cc54b1374c88c1b2dd67550f7a011d68c3179afb
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-04-07 07:11:18 +0700

    Update web.php

--- Elades_Web/routes/web.php
+++ Elades_Web/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\landingpage\LandingPageController;
+use App\Http\Controllers\login\LoginController;
+use App\Http\Controllers\login\ForgotController;
 use App\Http\Controllers\dashboard\DashboardController;
 use App\Http\Controllers\pengaduan\InfrastrukturController;
 use App\Http\Controllers\pengaduan\KeamananController;
@@ -15,6 +17,16 @@ Route::get('/', function () {
 // Landing Page
 Route::get('/landing_page', [LandingPageController::class, 'index'])->name('landingpage');
 
+
+// Login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+//forgot
+Route::get('/forgot-password', [ForgotController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('/forgot-password', [ForgotController::class, 'sendResetLinkEmail'])->name('password.email');
+
 // Dashboard
 Route::get('/dashboardd', function () {
     return view('dashboard.dashboardd');
@@ -22,6 +34,8 @@ Route::get('/dashboardd', function () {
 
 // Pengaduan
 Route::get('/infrastruktur', [InfrastrukturController::class, 'index'])->name('infras');
+Route::post('/infrastruktur/{id}/update-status', [InfrastrukturController::class, 'updateStatus']);
+
 Route::get('/keamanan', [KeamananController::class, 'index'])->name('keamanan');
 Route::get('/saran', [SaranController::class, 'index'])->name('saran');
 
