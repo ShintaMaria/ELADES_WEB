86a93f1fbe63f2d06000056de62cbcaeefb6d467
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-04-07 12:53:42 +0700

    Update forgot-password.blade.php

--- Elades_Web/resources/views/login/forgot-password.blade.php
+++ Elades_Web/resources/views/login/forgot-password.blade.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
-    <title>Kauman Nganjuk - Forgot Password</title>
+    <link href="{{ asset('landingpage/assets/img/logonavbar.png')}}" rel="icon">
+    <title>Forgot Password - Kauman Nganjuk</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('dashboard/assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
