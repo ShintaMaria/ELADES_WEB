5ec75040ab3b20752fc273e7a54854b230a64549
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-04-07 20:14:28 +0700

    Create 2025_04_07_105719_create_statistik_table.php

---
+++ Elades_Web/database/migrations/2025_04_07_105719_create_statistik_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('statistik', function (Blueprint $table) {
+            $table->id();
+            $table->integer('total_jiwa');
+            $table->integer('jumlah_kk');
+            $table->integer('jumlah_dusun');
+            $table->float('luas_wilayah'); // misalnya luas dalam km²
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statistik');
+    }
+};
