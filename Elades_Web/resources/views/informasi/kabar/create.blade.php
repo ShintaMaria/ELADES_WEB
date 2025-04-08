1eb4df31a8146f7c36d231ea1b25a2ee7f9b6467
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-04-08 11:35:57 +0700

    Create create.blade.php

---
+++ Elades_Web/resources/views/informasi/kabar/create.blade.php
@@ -0,0 +1,27 @@
+@extends('dashboard.layouts.template')
+@section('content')
+<div class="container-fluid">
+    <h1>Tambah Kabar Desa</h1>
+    <form action="{{ route('kabardesa.store') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="mb-3">
+            <label>Judul</label>
+            <input type="text" name="judul" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label>Deskripsi</label>
+            <textarea name="deskripsi" class="form-control" required></textarea>
+        </div>
+        <div class="mb-3">
+            <label>Tanggal</label>
+            <input type="date" name="tanggal" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label>Gambar</label>
+            <input type="file" name="gambar" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+        <a href="{{ route('kabardesa.index') }}" class="btn btn-secondary">Kembali</a>
+    </form>
+</div>
+@endsection
