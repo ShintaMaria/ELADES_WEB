152ff0594904178e41784c797a0709ceb7be4f4c
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-04-09 20:27:19 +0700

    Create topbar2.blade.php

---
+++ Elades_Web/resources/views/dashboard/layouts/topbar2.blade.php
@@ -0,0 +1,22 @@
+ <!-- Content Wrapper -->
+ <div id="content-wrapper" class="d-flex flex-column">
+
+<!-- Main Content -->
+<div id="content">
+
+    <!-- Topbar -->
+    <nav class="navbar navbar-expand navbar-light bg-gradient-primary topbar mb-4 shadow">
+            <div class="container-fluid">
+                <a href="{{ route('dashboard') }}" class="text-white d-flex align-items-center" style="font-size: 1.2rem; font-weight: bold; text-decoration: none;">
+                    <i class="fas fa-arrow-left" style="margin-right: 10px;"></i>
+                    <span>Kembali</span>
+                </a>
+            </div>
+        </nav>
+
+
+
+
+</div>
+
+    
