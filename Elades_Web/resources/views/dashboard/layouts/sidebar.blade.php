6be83a8310e30908719a54f08a7e9b09bf366eec
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-05-01 15:55:40 +0700

    Update

--- Elades_Web/resources/views/dashboard/layouts/sidebar.blade.php
+++ Elades_Web/resources/views/dashboard/layouts/sidebar.blade.php
@@ -37,9 +37,10 @@
             <h6 class="collapse-header">Surat Pengantar</h6>
             <a class="collapse-item" href="{{ route('skck') }}">Pengantar SKCK</a>
             <a class="collapse-item" href="cards.html">Kehilangan Barang</a>
+            <a class="collapse-item" href="{{ route('sktm') }}">Pengantar SKTM</a>
             <h6 class="collapse-header">Surat Keterangan</h6>
             <a class="collapse-item" href="buttons.html">SKTM</a>
-            <a class="collapse-item" href="cards.html">Penghasilan Orang Tua</a>
+            <a class="collapse-item" href="{{ route('penghasilan') }}">Keterangan Penghasilan</a>
             <h6 class="collapse-header">Surat Izin</h6>
             <a class="collapse-item" href="buttons.html">Izin Tidak Masuk Kerja</a>
             <a class="collapse-item" href="cards.html">Izin Tempat Usaha</a>
