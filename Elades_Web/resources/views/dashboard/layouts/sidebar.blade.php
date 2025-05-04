bfb3458302cbfb2c1e2b4f8ea12a756da74e3be7
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-05-04 18:45:27 +0700

    perbaiki error

--- Elades_Web/resources/views/dashboard/layouts/sidebar.blade.php
+++ Elades_Web/resources/views/dashboard/layouts/sidebar.blade.php
@@ -1,71 +1,69 @@
 <!-- Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-<!-- Sidebar - Brand -->
-<a class="sidebar-brand d-flex align-items-center justify-content-center" >
-<img src="{{ asset('landingpage/assets/img/logonavbar.png')}}" alt="" width="70" height="auto">
-    <div class="sidebar-brand-text mx-3">Kauman </br> Nganjuk</div>
-</a>
-
-
-<!-- Divider -->
-<hr class="sidebar-divider my-0">
-
-<!-- Nav Item - Dashboard -->
-<li class="nav-item active">
-    <a class="nav-link" href="{{ route('dashboard') }}">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Dashboard</span></a>
-</li>
-
-<!-- Divider -->
-<hr class="sidebar-divider">
-
-<!-- Heading -->
-<div class="sidebar-heading">
-    Layanan Administrasi
-</div>
-<!-- Nav Item - Pages Collapse Menu -->
-<li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePembuatanSurat"
-        aria-expanded="true" aria-controls="collapsePembuatanSurat">
-        <i class="fas fa-file-alt"></i>
-        <span>Pembuatan Surat</span>
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center">
+        <img src="{{ asset('landingpage/assets/img/logonavbar.png')}}" alt="" width="70" height="auto">
+        <div class="sidebar-brand-text mx-3">Kauman <br> Nganjuk</div>
     </a>
-    <div id="collapsePembuatanSurat" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Surat Pengantar</h6>
-            <a class="collapse-item" href="{{ route('skck') }}">Pengantar SKCK</a>
-<<<<<<< HEAD
-            <a class="collapse-item" href="cards.html">Kehilangan Barang</a>
-            <a class="collapse-item" href="{{ route('sktm') }}">Pengantar SKTM</a>
-=======
-            <a class="collapse-item" href="{{ route('kehilangan') }}">Kehilangan Barang</a>
->>>>>>> f1a33bec875c42393996cd2610314019dad8262e
-            <h6 class="collapse-header">Surat Keterangan</h6>
-            <a class="collapse-item" href="buttons.html">SKTM</a>
-            <a class="collapse-item" href="{{ route('penghasilan') }}">Keterangan Penghasilan</a>
-            <h6 class="collapse-header">Surat Izin</h6>
-            <a class="collapse-item" href="buttons.html">Izin Tidak Masuk Kerja</a>
-            <a class="collapse-item" href="{{ route('keramaian') }}">Izin Keramaian</a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">Layanan Administrasi</div>
+
+    <!-- Pembuatan Surat -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePembuatanSurat"
+            aria-expanded="true" aria-controls="collapsePembuatanSurat">
+            <i class="fas fa-file-alt"></i>
+            <span>Pembuatan Surat</span>
+        </a>
+        <div id="collapsePembuatanSurat" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Surat Pengantar</h6>
+                <a class="collapse-item" href="{{ route('skck') }}">Pengantar SKCK</a>
+                <a class="collapse-item" href="{{ route('kehilangan') }}">Kehilangan Barang</a>
+                <a class="collapse-item" href="{{ route('sktm') }}">Pengantar SKTM</a>
+
+                <h6 class="collapse-header">Surat Keterangan</h6>
+                <a class="collapse-item" href="#">SKTM</a> <!-- Belum ada route? -->
+                <a class="collapse-item" href="{{ route('penghasilan') }}">Keterangan Penghasilan</a>
+
+                <h6 class="collapse-header">Surat Izin</h6>
+                <a class="collapse-item" href="#">Izin Tidak Masuk Kerja</a> <!-- Belum ada route? -->
+                <a class="collapse-item" href="{{ route('keramaian') }}">Izin Keramaian</a>
+            </div>
         </div>
-    </div>
-</li>
+    </li>
 
-<!-- Laporan Surat -->
-<li class="nav-item">
-    <a class="nav-link" href="cards.html">
-        <i class="fas fa-clipboard-list"></i>
-        <span>Laporan</span></a>
-</li>
+    <!-- Laporan Surat -->
+    <li class="nav-item">
+        <a class="nav-link" href="cards.html">
+            <i class="fas fa-clipboard-list"></i>
+            <span>Laporan</span>
+        </a>
+    </li>
 
-<!-- Divider -->
-<hr class="sidebar-divider">
- <!-- Heading -->
-    <div class="sidebar-heading">
-    Layanan Aspirasi
-    </div>
-    <!-- Nav Item - Pages Collapse Menu -->
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">Layanan Aspirasi</div>
+
+    <!-- Pengaduan -->
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseInsfrastruktur"
             aria-expanded="true" aria-controls="collapseInsfrastruktur">
@@ -80,51 +78,52 @@
             </div>
         </div>
     </li>
-<<<<<<< HEAD
-    
-    <!-- Laporan Surat -->
-=======
 
->>>>>>> f1a33bec875c42393996cd2610314019dad8262e
+    <!-- Laporan Pengaduan (jika perlu pisah dari laporan surat) -->
     <li class="nav-item">
         <a class="nav-link" href="cards.html">
             <i class="fas fa-clipboard-list"></i>
-            <span>Laporan</span></a>
+            <span>Laporan</span>
+        </a>
     </li>
 
-<!-- Heading -->
-<hr class="sidebar-divider">
-<div class="sidebar-heading">
-    Informasi
-</div>
+    <!-- Divider -->
+    <hr class="sidebar-divider">
 
-<!-- Nav Item - Charts -->
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('kabardesa') }}">
-        <i class="fas fa-newspaper"></i>
-        <span>Kabar Desa</span></a>
-</li>
+    <!-- Heading -->
+    <div class="sidebar-heading">Informasi</div>
 
-<!-- Nav Item - Tables -->
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('artikel') }}">
-        <i class="fas fa-book-open"></i>
-        <span>Artikel Terkini</span></a>
-</li>
+    <!-- Kabar Desa -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('kabardesa') }}">
+            <i class="fas fa-newspaper"></i>
+            <span>Kabar Desa</span>
+        </a>
+    </li>
 
-<!-- Nav Item - Tables -->
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('statistik') }}">
-        <i class="fas fa-chart-bar"></i>
-        <span>Data Statistik</span></a>
-</li>
+    <!-- Artikel -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('artikel') }}">
+            <i class="fas fa-book-open"></i>
+            <span>Artikel Terkini</span>
+        </a>
+    </li>
 
-<!-- Divider -->
-<hr class="sidebar-divider d-none d-md-block">
+    <!-- Statistik -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('statistik') }}">
+            <i class="fas fa-chart-bar"></i>
+            <span>Data Statistik</span>
+        </a>
+    </li>
 
-<!-- Sidebar Toggler (Sidebar) -->
-<div class="text-center d-none d-md-inline">
-    <button class="rounded-circle border-0" id="sidebarToggle"></button>
-</div>
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
 </ul>
 <!-- End of Sidebar -->
--- Elades_Web/routes/web.php
+++ Elades_Web/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\landingpage\LandingPageController;
 use App\Http\Controllers\login\LoginController;
@@ -15,12 +16,9 @@ use App\Http\Controllers\informasi\StatistikController;
 use App\Http\Controllers\dashboard\ProfileController;
 use App\Http\Controllers\pengaduan\LaporanSKCKController;
 use App\Http\Controllers\surat\SkckController;
-<<<<<<< HEAD
 use App\Http\Controllers\surat\SktmController;
 use App\Http\Controllers\surat\PenghasilanController;
-=======
 use App\Http\Controllers\TampilanSuratController;
->>>>>>> f1a33bec875c42393996cd2610314019dad8262e
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,7 +26,6 @@ Route::get('/', function () {
 
 // Landing Page
 Route::get('/landing_page', [LandingPageController::class, 'index'])->name('landingpage');
-
 
 // Login
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
@@ -38,76 +35,69 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name('google.login');
 Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
 
-//forgot
+// Forgot Password
 Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
 Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
 Route::get('/reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
 Route::post('/reset-password', [ResetPasswordController::class, 'reset'])->name('password.update');
 
-// route yang hanya bisa diakses saat sudah login
+// Route yang hanya bisa diakses saat sudah login
 Route::middleware('auth')->group(function () {
-    
-    // dashboard
+
+    // Dashboard
     Route::get('/dashboardd', function () {
         return view('dashboard.dashboardd');
     })->name('dashboard');
 
-    // pengaduan
+    // Pengaduan
     Route::get('/infrastruktur', [InfrastrukturController::class, 'index'])->name('infras');
     Route::post('/infrastruktur/{id}/update-status', [InfrastrukturController::class, 'updateStatus']);
     Route::get('/keamanan', [KeamananController::class, 'index'])->name('keamanan');
     Route::get('/saran', [SaranController::class, 'index'])->name('saran');
 
-    // kabar desa
+    // Kabar Desa
     Route::get('/kabar_desa', [KabarDesaController::class, 'index'])->name('kabardesa');
     Route::resource('kabardesa', KabarDesaController::class);
 
-    // artikel terkini
+    // Artikel Terkini
     Route::get('/artikel_terkini', [ArtikelTerkiniController::class, 'index'])->name('artikel');
     Route::resource('artikels', ArtikelTerkiniController::class);
 
-    // data statistik
+    // Statistik
     Route::get('/statistik', [StatistikController::class, 'index'])->name('statistik');
     Route::get('/statistik/edit', [StatistikController::class, 'edit'])->name('statistik.edit');
     Route::put('/statistik/update', [StatistikController::class, 'update'])->name('statistik.update');
 
-    // profil pengguna
+    // Profile
     Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
     Route::put('/profile/photo', [ProfileController::class, 'updatePhoto'])->name('profile.updatePhoto');
     Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.updatePassword');
     Route::delete('/profile/photo', [ProfileController::class, 'deletePhoto'])->name('profile.deletePhoto');
 
-<<<<<<< HEAD
-    // pengajuan surat
+    // Pengajuan Surat
     Route::get('/skck', [SkckController::class, 'index'])->name('skck');
     Route::get('/sktm', [SktmController::class, 'index'])->name('sktm');
     Route::get('/penghasilan', [PenghasilanController::class, 'index'])->name('penghasilan');
 
-    // laporan pengajuan
+    // Laporan Pengajuan
     Route::get('/laporan-skck', [LaporanSKCKController::class, 'index'])->name('laporan-skck.index');
     Route::post('/laporan-skck/{id}/update-status', [LaporanSKCKController::class, 'updateStatus']);
 
+    // Tampilan Pengajuan Surat
+    Route::get('/keramaian', [TampilanSuratController::class, 'keramaian'])->name('keramaian');
+    Route::get('/kehilangan-barang', [TampilanSuratController::class, 'kehilangan'])->name('kehilangan');
+
+    // Detail Pengajuan Surat
+    Route::get('/skck/{id}', [SkckController::class, 'show'])->name('skck.show');
+    Route::get('/kehilangan/{id}', [SkckController::class, 'show'])->name('kehilangan.show');
+    Route::get('/keramaian/{id}', [SkckController::class, 'show'])->name('keramaian.show');
+
+    Route::post('/skck/{id}/selesai', [SkckController::class, 'selesai'])->name('skck.selesai');
+    Route::post('/kehilangan/{id}/selesai', [SkckController::class, 'selesai'])->name('kehilangan.selesai');
+    Route::post('/keramaian/{id}/selesai', [SkckController::class, 'selesai'])->name('keramaian.selesai');
+
+    Route::post('/skck/{id}/tolak', [SkckController::class, 'tolak'])->name('skck.tolak');
+    Route::post('/kehilangan/{id}/tolak', [SkckController::class, 'tolak'])->name('kehilangan.tolak');
+    Route::post('/keramaian/{id}/tolak', [SkckController::class, 'tolak'])->name('keramaian.tolak');
     
-});// lek nambah route, tambahin di atas ini ya gaes, biar ikut kebungkus middleware (cuma bisa diakses kalau sudah login)
-=======
-//Pengajuan Surat
-Route::get('/skck', [TampilanSuratController::class, 'skck'])->name('skck');
-Route::get('/keramaian', [TampilanSuratController::class, 'keramaian'])->name('keramaian');
-Route::get('/kehilangan-barang', [TampilanSuratController::class, 'kehilangan'])->name('kehilangan');
-
-//Detail Pengajuan Surat
-Route::get('/skck/{id}', [SkckController::class, 'show'])->name('skck.show');
-Route::get('/kehilangan/{id}', [SkckController::class, 'show'])->name('kehilangan.show');
-Route::get('/keramaian/{id}', [SkckController::class, 'show'])->name('keramaian.show');
-
-
-Route::post('/skck/{id}/selesai', [SkckController::class, 'selesai'])->name('skck.selesai');
-Route::post('/kehilangan/{id}/selesai', [SkckController::class, 'selesai'])->name('kehilangan.selesai');
-Route::post('/keramaian/{id}/selesai', [SkckController::class, 'selesai'])->name('keramaian.selesai');
-
-Route::post('/skck/{id}/tolak', [SkckController::class, 'tolak'])->name('skck.tolak');
-Route::post('/kehilangan/{id}/tolak', [SkckController::class, 'tolak'])->name('kehilangan.tolak');
-Route::post('/keramaian/{id}/tolak', [SkckController::class, 'tolak'])->name('keramaian.tolak');
-
-//
->>>>>>> f1a33bec875c42393996cd2610314019dad8262e
+}); // Lek nambah route, tambahin di atas ini ya gaes, biar ikut kebungkus middleware (cuma bisa diakses kalau sudah login)
