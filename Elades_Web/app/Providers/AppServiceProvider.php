e41a1f02f5c8c26933255fd97f9c75c377bdd9f7
shazidian <158548646+shazidian@users.noreply.github.com>
2025-05-19 02:30:32 +0700

    provider notif

--- Elades_Web/app/Providers/AppServiceProvider.php
+++ Elades_Web/app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+
     }
 
     /**
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Carbon::setLocale('id');
+        // Ambil data notifikasi
+        $notifikasi = DB::table('notifikasi_web')->orderBy('tanggal', 'desc')->get();
+        $jumlahNotifikasi = $notifikasi->count();
+
+        // Bagikan ke semua view
+        View::share('notifikasi', $notifikasi);
+        View::share('jumlahNotifikasi', $jumlahNotifikasi);
     }
 }
