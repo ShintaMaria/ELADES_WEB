56ad91750215c8bd534e635cf239eb95c3e79522
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-05-19 11:42:06 +0700

    template unduh laporan

--- Elades_Web/resources/views/pengaduan/laporan_pengaduan_pdf.blade.php
+++ Elades_Web/resources/views/pengaduan/laporan_pengaduan_pdf.blade.php
@@ -8,28 +8,83 @@
             font-family: sans-serif;
             font-size: 12px;
         }
-        h2 {
-            text-align: center;
+
+        .kop-surat {
+            position: relative;
+            margin-bottom: 10px;
         }
+
+        .kop-surat img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100px;
+            height: 100px;
+        }
+
+        .kop-surat .text {
+            text-align: center;
+            margin-left: 0;
+            padding-left: 10px; /* spasi dari kiri agar tidak ketabrak logo */
+        }
+
+        .kop-surat .text h1,
+        .kop-surat .text h2,
+        .kop-surat .text h3 {
+            margin: 0;
+            line-height: 1.2;
+        }
+
+        hr {
+            border: 1px solid #000;
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+
+        .judul-laporan {
+            text-align: center;
+            margin-top: 20px;
+            font-weight: bold;
+        }
+
         table {
             width: 100%;
             border-collapse: collapse;
             margin-top: 20px;
         }
+
         table, th, td {
             border: 1px solid #333;
         }
+
         th, td {
             padding: 6px;
             text-align: left;
         }
+
         th {
             background-color: #f0f0f0;
         }
     </style>
 </head>
 <body>
-    <h2>Laporan Pengaduan - Bulan {{ $bulan }} Tahun {{ $tahun }}</h2>
+
+    <div class="kop-surat">
+        <img src="landingpage/assets/img/logonavbar.png" alt="Logo">
+        <div class="text">
+            <h2>PEMERINTAH KABUPATEN NGANJUK</h2>
+            <h3>KECAMATAN NGANJUK</h3>
+            <h3>DESA KAUMAN</h3>
+            <p>Alamat: Jl. Gatot Subroto 100, Kode Pos: 64411</p>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="judul-laporan">
+        Laporan Pengaduan Masyarakat - Bulan {{ $bulan }} Tahun {{ $tahun }}
+    </div>
+
     <table>
         <thead>
             <tr>
@@ -56,5 +111,6 @@
             @endforelse
         </tbody>
     </table>
+
 </body>
 </html>
